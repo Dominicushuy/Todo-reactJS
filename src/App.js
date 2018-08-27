b3df1041961b70e61eb859b1a9d0f1598cc406f1
Huy <dominicushuy@gmail.com>
2018-08-27 23:09:17 +0700

    update

--- src/App.js
+++ src/App.js
@@ -26,6 +26,13 @@ class App extends Component {
       sortValue : 1 //1 tang , -1: giam
     }
   }
+  s4 = () => {
+    return Math.floor((1+Math.random()) * 0x10000 ).toString(16).substring(1); 
+  }
+
+  generateID = ()=>{
+    return s4() + s4() + '-' + s4() + s4() + '-' +s4()
+  }
 
   onGetData = () =>{
     let { data } = this.props;
